8137e709bbad27ea5c40e053cd9b766adf3bd8dc
kananbayramli <bayramli-kenan@mail.ru>
2024-03-09 18:54:41 +0400

    Data Seeding

--- StoreApp.Data/Concrete/StoreDbContext.cs
+++ StoreApp.Data/Concrete/StoreDbContext.cs
@@ -10,4 +10,22 @@ public class StoreDbContext : DbContext
     }
 
     public DbSet<Product> Products => Set<Product>();
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Product>().HasData(
+            new List<Product>(){
+                new () {Id=1,Name="Samsung S24",Description="Cox gozel telefon",Price=1500,Category="Telefon"},                   
+                new () {Id=2,Name="Samsung S25",Description="Cox gozel telefon",Price=2000,Category="Telefon"},                           
+                new () {Id=3,Name="Samsung S26",Description="Cox gozel telefon",Price = 2500,Category = "Telefon"},
+                new () {Id=4,Name="IPhone 11 pro",Description="Cox gozel telefon",Price=2500,Category="Telefon"},
+                new () {Id=5,Name="IPhone 12 pro Max",Description="Cox gozel telefon",Price=2800,Category="Telefon"},
+                new () {Id=6,Name="IPhone 15 pro Max",Description="Cox serfeli telefon",Price=3800,Category="Telefon"}
+            }
+        );
+
+    }
 }
---
+++ StoreApp.Web/Migrations/20240309144934_InitialCreate.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace StoreApp.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Category = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Category", "Description", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Telefon", "Cox gozel telefon", "Samsung S24", 1500m },
+                    { 2, "Telefon", "Cox gozel telefon", "Samsung S25", 2000m },
+                    { 3, "Telefon", "Cox gozel telefon", "Samsung S26", 2500m },
+                    { 4, "Telefon", "Cox gozel telefon", "IPhone 11 pro", 2500m },
+                    { 5, "Telefon", "Cox gozel telefon", "IPhone 12 pro Max", 2800m },
+                    { 6, "Telefon", "Cox serfeli telefon", "IPhone 15 pro Max", 3800m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}

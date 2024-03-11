050e95a44d94a2a4f4b386493c23c5ed775ea2ce
kananbayramli <bayramli-kenan@mail.ru>
2024-03-11 15:49:58 +0400

    AutoMappper

--- StoreApp.Web/Controllers/HomeController.cs
+++ StoreApp.Web/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using StoreApp.Data.Abstract;
@@ -9,24 +10,19 @@ namespace StoreApp.Web.Controllers;
 public class HomeController : Controller
 {
     public int pageSize = 3;
-    private IStoreRepository _storeRepository;
-    public HomeController(IStoreRepository storeRepository)
+    private readonly IStoreRepository _storeRepository;
+    private readonly IMapper _mapper;
+    public HomeController(IStoreRepository storeRepository, IMapper mapper)
     {
         _storeRepository = storeRepository;
-    }
-    
-    
+        _mapper = mapper;
+    }  
 
     public IActionResult Index(string category, int page = 1)
     {
         return View(new ProductListViewModel{
-            Products = _storeRepository.GetProductsByCategory(category, page, pageSize).Select(p =>                        //auto mapper yazacam
-                new ProductViewModel{
-                    Id = p.Id,  
-                    Price = p.Price,
-                    Name = p.Name,
-                    Description = p.Description
-                }),
+            Products = _storeRepository.GetProductsByCategory(category, page, pageSize)
+            .Select(product => _mapper.Map<ProductViewModel>(product)),
 
             PageInfo = new PageInfo{
                 CurrentPage = page,

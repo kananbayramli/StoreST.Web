8b275bcddcabe8e404dea3b67aecd195a282be37
kananbayramli <bayramli-kenan@mail.ru>
2024-03-14 16:07:10 +0400

    CartModel

--- StoreApp.Web/obj/Debug/net8.0/StoreApp.Web.AssemblyInfo.cs
+++ StoreApp.Web/obj/Debug/net8.0/StoreApp.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+050e95a44d94a2a4f4b386493c23c5ed775ea2ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e97b2e24f7d1a58d4c840e014f7f1fbdbafa7c3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
